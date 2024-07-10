109443020284f251d4157c726cbd882e957eb54c
alaeddine <baraalaeddine@gmail.com>
2024-07-10 23:22:50 +0100

    new sign in/up

--- src/Sign/Sign.js
+++ src/Sign/Sign.js
@@ -1,38 +1,57 @@
-// Sign.js
-
 import React, { useState } from 'react';
-import styles from './Sign.module.css';
-import Sign_up from './Sign_in_up_component/Sign_up';
-import Sign_in from './Sign_in_up_component/Sign_in';
+import styles from './Sign.module.css'; 
+import SignInForm from './Sign_in_up_component/Sign_in';
+import SignUpForm from './Sign_in_up_component/Sign_up';
+import dda from './log.svg';
+import ddaa from './register.svg';
 
-const Sign = () => {
-  const [isSignUp, setIsSignUp] = useState(false);
+const AuthContainer = () => {
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
 
-  const toggleForm = () => {
-    setIsSignUp(prevState => !prevState);
+  const handleSignUpClick = () => {
+    setIsSignUpMode(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpMode(false);
   };
 
   return (
-    <>
-      <div className={styles.SignContainer}>
-        <div className={styles.SwitchContainer}>
-          <label className={styles.switch}>
-            <input
-              type="checkbox"
-              className={`${styles.toggle} toggle`}
-              checked={isSignUp}
-              onChange={toggleForm}
-            />
-            <span className={styles.slider}></span>
-            <div className={styles.sliderLabel}>{isSignUp ? 'Sign up' : 'Sign in'}</div>
-          </label>
+    <div className={`${styles.container} ${isSignUpMode ? styles['sign-up-mode'] : ''}`}>
+      <div className={styles['forms-container']}>
+        <div className={styles['signin-signup']}>
+          <SignInForm />
+          <SignUpForm />
         </div>
       </div>
-
-      {isSignUp ? <Sign_up /> : <Sign_in />}
-    </>
-    
+      <div className={styles['panels-container']}>
+        <div className={`${styles.panel} ${styles['left-panel']}`}>
+          <div className={styles.content}>
+            <h3>New to our ebook library?</h3>
+            <p>
+              Discover a world of knowledge and entertainment with our extensive collection of ebooks. Join us today and start your reading journey!
+            </p>
+            <button className={`${styles.btn} ${styles.transparent}`} onClick={handleSignUpClick}>
+              Sign up
+            </button>
+          </div>
+          <img src={dda} className={styles.image} alt="Reading" />
+        </div>
+        <div className={`${styles.panel} ${styles['right-panel']}`}>
+          <div className={styles.content}>
+            <h3>Already a member?</h3>
+            <p>
+              Welcome back! Sign in to continue exploring our collection and enjoy personalized recommendations based on your reading history.
+            </p>
+            <button className={`${styles.btn} ${styles.transparent}`} onClick={handleSignInClick}>
+              Sign in
+            </button>
+          </div>
+          <img src={ddaa} className={styles.image} alt="Member" />
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Sign;
+export default AuthContainer;
