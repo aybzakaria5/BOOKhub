4832aea1b8be8bd80895f2213f5c0f0bcbc441be
alaeddine <baraalaeddine@gmail.com>
2024-07-10 12:55:48 +0100

    Sign done

--- src/Sign/Sign.js
+++ src/Sign/Sign.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import Style from './Sign.module.css'
-import Sign_up from './Sign_in_up_component/Sign_up'
-import Sign_in from './Sign_in_up_component/Sign_in'
+// Sign.js
+
+import React, { useState } from 'react';
+import styles from './Sign.module.css';
+import Sign_up from './Sign_in_up_component/Sign_up';
+import Sign_in from './Sign_in_up_component/Sign_in';
 
 const Sign = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const toggleForm = () => {
+    setIsSignUp(prevState => !prevState);
+  };
+
   return (
     <>
+      <div className={styles.SignContainer}>
+        <div className={styles.SwitchContainer}>
+          <label className={styles.switch}>
+            <input
+              type="checkbox"
+              className={`${styles.toggle} toggle`}
+              checked={isSignUp}
+              onChange={toggleForm}
+            />
+            <span className={styles.slider}></span>
+            <div className={styles.sliderLabel}>{isSignUp ? 'Sign up' : 'Sign in'}</div>
+          </label>
+        </div>
+      </div>
 
-
-     
-    
-    
+      {isSignUp ? <Sign_up /> : <Sign_in />}
     </>
-  )
-}
+    
+  );
+};
 
-export default Sign
+export default Sign;
