fdd61748759bf7ad4b43ca489b521a959c1530ef
alaeddine <baraalaeddine@gmail.com>
2024-07-12 10:14:28 +0100

    design page add book done

--- src/Add_book/Add.js
+++ src/Add_book/Add.js
@@ -1,61 +1,59 @@
 import React from 'react';
 import Style from './Add.module.css';
-import { FileInput, Select} from "flowbite-react";
+import { FileInput, Select } from "flowbite-react";
 
 const Add = () => {
   return (
-    <>
-        <div className={Style.background}>
-            <div className={Style.content}>
-            <h1>Add book </h1>
-            {/* <p>Questions, bug reports, feedback, feature requests — we're here for it all.</p> */}
-            </div>
+    <div className={Style.container}>
+      <div className={Style.formContainer}>
+        <div className={Style.Content}>
+            <h1>Add your book</h1>
+            <p>Get started by adding your book to our library. You can add as many books as you'd like.</p>
         </div>
+        <form className={Style.contactForm}>
+          <label className={Style.fileLabel}>
+            Cover image
+            <FileInput id="file-upload-helper-text" accept="image/png, image/jpeg" helperText="File accepted: PNG, JPG." />
+          </label>
 
-        <div className={Style.divForm}>
-            <form className={Style.contactForm} >
+          <label>
+            Title
+            <input type="text" required />
+          </label>
 
-            <label className={Style.filee}>
-                Cover image
-                <FileInput id="file-upload-helper-text"  accept="image/png, image/jpeg"  helperText="File accepted :PNG, JPG." />
-            </label>
+          <label>
+            Category
+            <Select id={Style.category_select} required>
+              <option value="" disabled selected>Select a category</option>
+              <option>Adventure</option>
+              <option>Romance</option>
+              <option>Thriller</option>
+              <option>Memoir</option>
+              <option>Travel</option>
+              <option>Health</option>
+              <option>Poetry</option>
+              <option>Cooking</option>
+            </Select>
+          </label>
 
-            <label>
-                Title 
-                <input type="text" required />
-            </label>
+          <label>
+            Author
+            <input type="text" required />
+          </label>
 
-            <label>
-                Categorie 
-                <Select id={Style.category_select} required >
-                    <option>Adventure</option>
-                    <option>Romance</option>
-                    <option>Thriller</option>
-                    <option>Memoir</option>
-                    <option>Travel</option>
-                    <option>Health</option>
-                    <option>Poetry</option>
-                    <option>Cooking</option>
-                </Select>
-            </label>
-            
+          <label>
+            Description
+            <textarea required></textarea>
+          </label>
 
-            <label>
-                author
-                <input type="text" required />
-            </label>
-
-            <label>
-                Description
-                <textarea required></textarea>
-            </label>
-            <button type="submit">Add</button>
-            </form>
-        </div>
-    
-    
-    </>
-  )
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+      <div className={Style.imageContainer}>
+        <img src="https://d2616tuem1neks.cloudfront.net/misc_assets/press/Photography/Ereader.jpg" alt="E-reader" />
+      </div>
+    </div>
+  );
 }
 
-export default Add
+export default Add;
